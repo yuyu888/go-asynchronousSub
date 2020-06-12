8c9e96230e4ebcbb192a90d58165346b861a5cf6
yuyu888 <104250965@qq.com>
2020-06-12 20:40:12 +0800

    ‘格式话’

--- libs/log/logger.go
+++ libs/log/logger.go
@@ -1,17 +1,19 @@
 package log
 
-
-
 import (
-    "go.uber.org/zap"
-    "go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
+// InfoLogger info日志类型
 var InfoLogger *zap.Logger
+
+// DebugLogger debug日志类型
 var DebugLogger *zap.Logger
 
+// Init 初始化方法
 func Init() {
 
-    InfoLogger = NewLogger("./info.log", zapcore.InfoLevel, 128, 30, 7, true, "Info")
-    DebugLogger = NewLogger("./debug.log", zapcore.DebugLevel, 128, 30, 7, true, "Debug")
+	InfoLogger = NewLogger("./info.log", zapcore.InfoLevel, 128, 30, 7, true, "Info")
+	DebugLogger = NewLogger("./debug.log", zapcore.DebugLevel, 128, 30, 7, true, "Debug")
 }
